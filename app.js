30415116893131d66f721613416b040e4dd8dfcc
ElDez28 <dezmic91@gmail.com>
2023-02-22 16:41:20 +0100

    cors

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const helmet = require("helmet");
 const reviewRouter = require("./reviewRouter/reviewRouter");
 const dotenv = require("dotenv");
 dotenv.config({ path: `${__dirname}/config.env` });
+
 app.use(
   cors({
     credentials: true,
     origin: true,
   })
 );
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Authorization"
+  );
+  next();
+});
 app.use(express.json());
 app.use(
   helmet({
